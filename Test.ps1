13f0c6d80d31bfb240b6c67d3d5f5f403c7efdb3
justkaz16 <kaz@justkaz.co.uk>
2018-01-16 14:38:27 +0000

    AppService with BLOB Storage

---
+++ Test.ps1
@@ -0,0 +1,37 @@
+﻿cls
+
+dir
+
+cd 
+
+get-psdrive
+
+
+get-psprovider
+
+set-location 
+
+
+get-service
+
+
+get-process
+
+
+get-process 
+
+
+get-Wmiobject Win32_bios
+
+
+Get-WmiObject win32_processor
+
+
+Get-AzureRmResourceProvider
+
+
+Get-Import-Module AzureRM.Resources
+
+
+
+
